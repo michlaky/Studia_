220bf2e5a302df4428bc9d435ac319e33f7f2397
michlaky <64215633+michlaky@users.noreply.github.com>
2024-06-23 19:46:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
-import flask from flask
-import streamlit as st
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-if __name__ == '__main__':
-    app.run(debug=True)
-
 st.title("Moja pierwsza aplikacja Streamlit")
 st.write("Witaj w mojej aplikacji Streamlit!")
 
