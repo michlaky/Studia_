aede1b921a456e0b2b05b76d0e825912dab9cfe0
michlaky <64215633+michlaky@users.noreply.github.com>
2024-06-23 19:35:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+#zad 1
 
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-if __name__ == '__main__':
-    app.run(debug=True)
+a= int(input("Podaj wartość zmiennej a: "))
+b=int(input("Podaj wartość zmiennej b: "))
+if a >= 0 and a < 10 and b >= 0 and b < 10:
+ il=a*b
+ print("Iloczyn wynosi: " , il)
+else:
+ print("Dane poza zakresem od 0 do 9!")
+ 
+ jezyk = input(str("Podaj nazwę ulubionego języka programowania"))
+if jezyk == "Python":
+    print("Zostań naukowcem od przetwarzania danych")
+elif jezyk == "Java":
+    print("Twórz aplikacje na urządzenia mobilne.")
+elif jezyk =="c#":
+    print("Stwórz grę multimedialną w Unity")
+elif jezyk =="PHP":
+    print("Możesz tworzyć strony intenetowe")
+elif jezyk =="SQL":
+    print("Rozwijaj systemy bazodanowe")
+elif jezyk =="":
+    print("Nie podales jezyka")
