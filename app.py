343dbeb4fc1303bcd521624269a5f9dd776918e2
michlaky <64215633+michlaky@users.noreply.github.com>
2024-06-23 19:41:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,7 @@
-#zad 1
+import streamlit as st
 
-a= int(input("Podaj wartość zmiennej a: "))
-b=int(input("Podaj wartość zmiennej b: "))
-if a >= 0 and a < 10 and b >= 0 and b < 10:
- il=a*b
- print("Iloczyn wynosi: " , il)
-else:
- print("Dane poza zakresem od 0 do 9!")
- 
- jezyk = input(str("Podaj nazwę ulubionego języka programowania"))
-if jezyk == "Python":
-    print("Zostań naukowcem od przetwarzania danych")
-elif jezyk == "Java":
-    print("Twórz aplikacje na urządzenia mobilne.")
-elif jezyk =="c#":
-    print("Stwórz grę multimedialną w Unity")
-elif jezyk =="PHP":
-    print("Możesz tworzyć strony intenetowe")
-elif jezyk =="SQL":
-    print("Rozwijaj systemy bazodanowe")
-elif jezyk =="":
-    print("Nie podales jezyka")
-if name == “main”:
-main()
+st.title("Moja pierwsza aplikacja Streamlit")
+st.write("Witaj w mojej aplikacji Streamlit!")
+
+name = st.text_input("Podaj swoje imię")
+st.write(f"Cześć, {name}!")
